e00c1198fcac754403a3905f5c6d29e2696d4fa9
pepisg <pedro.gonzalez@eia.edu.co>
2023-09-09 18:41:48 +0000

    Add simple gps waypoint logging node

--- nav2_gps_waypoint_follower_demo/setup.py
+++ nav2_gps_waypoint_follower_demo/setup.py
@@ -29,7 +29,8 @@ setup(
     entry_points={
         'console_scripts': [
             'logged_waypoint_follower = nav2_gps_waypoint_follower_demo.logged_waypoint_follower:main',
-            'interactive_waypoint_follower = nav2_gps_waypoint_follower_demo.interactive_waypoint_follower:main'
+            'interactive_waypoint_follower = nav2_gps_waypoint_follower_demo.interactive_waypoint_follower:main',
+            'gps_waypoint_logger = nav2_gps_waypoint_follower_demo.gps_waypoint_logger:main'
         ],
     },
 )
