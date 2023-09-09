6432b419b3ae6c298b17e9d9d3b29dc7856606f7
pepisg <pedro.gonzalez@eia.edu.co>
2023-09-09 17:27:17 +0000

    mapviz & rviz conditional launch

--- nav2_gps_waypoint_follower_demo/launch/gps_waypoint_follower.launch.py
+++ nav2_gps_waypoint_follower_demo/launch/gps_waypoint_follower.launch.py
@@ -17,8 +17,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 from nav2_common.launch import RewrittenYaml
 
 
@@ -33,6 +35,19 @@ def generate_launch_description():
     configured_params = RewrittenYaml(
         source_file=nav2_params, root_key="", param_rewrites="", convert_types=True
     )
+
+    use_rviz = LaunchConfiguration('use_rviz')
+    use_mapviz = LaunchConfiguration('use_mapviz')
+
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        'use_rviz',
+        default_value='False',
+        description='Whether to start RVIZ')
+
+    declare_use_mapviz_cmd = DeclareLaunchArgument(
+        'use_mapviz',
+        default_value='False',
+        description='Whether to start mapviz')
 
     gazebo_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -55,6 +70,18 @@ def generate_launch_description():
         }.items(),
     )
 
+    rviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_dir, "launch", 'rviz_launch.py')),
+        condition=IfCondition(use_rviz)
+    )
+
+    mapviz_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_dir, 'mapviz.launch.py')),
+        condition=IfCondition(use_mapviz)
+    )
+
     # Create the launch description and populate
     ld = LaunchDescription()
 
@@ -66,5 +93,11 @@ def generate_launch_description():
 
     # navigation2 launch
     ld.add_action(navigation2_cmd)
+
+    # viz launch
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(rviz_cmd)
+    ld.add_action(declare_use_mapviz_cmd)
+    ld.add_action(mapviz_cmd)
 
     return ld
