8e6649af6631d777dc7f81544606287b428c9b82
Sayer <mpsayer@hrlit2899.hrl.com>
2024-06-24 19:08:58 -0700

    Added Humble port of Pure Pursuit Controller
    Signed-off-by: Sayer <mpsayer@hrlit2899.hrl.com>
    Signed-off-by: Mitchell Sayer <mitchell4408@gmail.com>

---
+++ nav2_pure_pursuit_controller/include/nav2_pure_pursuit_controller/pure_pursuit_controller.hpp
@@ -0,0 +1,76 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ *  Author(s): Shrijit Singh <shrijitsingh99@gmail.com>
+ *
+ */
+
+#ifndef NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
+#define NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "nav2_core/controller.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "pluginlib/class_loader.hpp"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace nav2_pure_pursuit_controller
+{
+
+class PurePursuitController : public nav2_core::Controller
+{
+public:
+  PurePursuitController() = default;
+  ~PurePursuitController() override = default;
+
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+    std::string name, const std::shared_ptr<tf2_ros::Buffer> & tf,
+    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap_ros) override;
+
+
+  void cleanup() override;
+  void activate() override;
+  void deactivate() override;
+  void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+
+  geometry_msgs::msg::TwistStamped computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped & pose,
+    const geometry_msgs::msg::Twist & velocity
+    nav2_core::GoalChecker * goal_checker) override;
+
+  void setPlan(const nav_msgs::msg::Path & path) override;
+
+protected:
+  nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped & pose);
+
+  bool transformPose(
+    const std::shared_ptr<tf2_ros::Buffer> tf,
+    const std::string frame,
+    const geometry_msgs::msg::PoseStamped & in_pose,
+    geometry_msgs::msg::PoseStamped & out_pose,
+    const rclcpp::Duration & transform_tolerance
+  ) const;
+
+  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  std::string plugin_name_;
+  std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+  rclcpp::Logger logger_ {rclcpp::get_logger("PurePursuitController")};
+  rclcpp::Clock::SharedPtr clock_;
+
+  double desired_linear_vel_;
+  double lookahead_dist_;
+  double max_angular_vel_;
+  rclcpp::Duration transform_tolerance_ {0, 0};
+
+  nav_msgs::msg::Path global_plan_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_;
+};
+
+}  // namespace nav2_pure_pursuit_controller
+
+#endif  // NAV2_PURE_PURSUIT_CONTROLLER__PURE_PURSUIT_CONTROLLER_HPP_
