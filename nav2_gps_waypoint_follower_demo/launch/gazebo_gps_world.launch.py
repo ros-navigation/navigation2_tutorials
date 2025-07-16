114b265fb9394a7185a0e76dfce590795dfdb9e0
Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
2025-07-16 21:52:26 +0200

    fix licence
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>

--- nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
+++ nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Open Source Robotics Foundation
+# Copyright (c) 2018 Intel Corporation
 # Copyright (C) 2024 Stevedan Ogochukwu Omodolor Omodia
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
