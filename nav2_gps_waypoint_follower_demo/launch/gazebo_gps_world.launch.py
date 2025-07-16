d895fd51149d25691668db8a35bc52731275d87c
Stevedan Ogochukwu Omodolor <61468301+stevedanomodolor@users.noreply.github.com>
2025-07-16 23:00:06 +0200

    Feat/update gps tutorial (#115)
    * Gps navigation Tutorial update
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    * remove key
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    * spawn at highe location
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    * changes based on pr
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    * changes based on pr
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    * fix licence
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>
    
    ---------
    
    Signed-off-by: Stevedan Omodolor <stevedan.o.omodolor@gmail.com>

--- nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
+++ nav2_gps_waypoint_follower_demo/launch/gazebo_gps_world.launch.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2018 Intel Corporation
-#
+# Copyright (C) 2024 Stevedan Ogochukwu Omodolor Omodia
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,67 +16,90 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    ExecuteProcess,
+)
+
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # Get the launch directory
-    gps_wpf_dir = get_package_share_directory(
-        "nav2_gps_waypoint_follower_demo")
-    launch_dir = os.path.join(gps_wpf_dir, 'launch')
-    world = os.path.join(gps_wpf_dir, "worlds", "sonoma_raceway.world")
+    sim_dir = get_package_share_directory("nav2_minimal_tb3_sim")
+    tutorial_dir = get_package_share_directory("nav2_gps_waypoint_follower_demo")
 
-    urdf = os.path.join(gps_wpf_dir, 'urdf', 'turtlebot3_waffle_gps.urdf')
-    with open(urdf, 'r') as infp:
+    # Create the launch configuration variables
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    # Decalre the launch arguments
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    world_sdf = os.path.join(tutorial_dir, "worlds", "tb3_sonoma_raceway.sdf.xacro")
+    robot_sdf = os.path.join(sim_dir, "urdf", "gz_waffle_gps.sdf.xacro")
+
+    urdf = os.path.join(sim_dir, "urdf", "turtlebot3_waffle_gps.urdf")
+    with open(urdf, "r") as infp:
         robot_description = infp.read()
 
-    models_dir = os.path.join(gps_wpf_dir, "models")
-    models_dir += os.pathsep + \
-        f"/opt/ros/{os.getenv('ROS_DISTRO')}/share/turtlebot3_gazebo/models"
-    set_gazebo_model_path_cmd = None
+    gazebo_server = ExecuteProcess(
+        cmd=["gz", "sim", "-r", "-s", world_sdf],
+        output="screen",
+    )
 
-    if 'GAZEBO_MODEL_PATH' in os.environ:
-        gazebo_model_path = os.environ['GAZEBO_MODEL_PATH'] + \
-            os.pathsep + models_dir
-        set_gazebo_model_path_cmd = SetEnvironmentVariable(
-            "GAZEBO_MODEL_PATH", gazebo_model_path)
-    else:
-        set_gazebo_model_path_cmd = SetEnvironmentVariable(
-            "GAZEBO_MODEL_PATH", models_dir)
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"
+            )
+        ),
+        launch_arguments={"gz_args": ["-v4 -g "]}.items(),
+    )
 
-    set_tb3_model_cmd = SetEnvironmentVariable("TURTLEBOT3_MODEL", "waffle")
-
-    # Specify the actions
-    start_gazebo_server_cmd = ExecuteProcess(
-        cmd=['gzserver', '-s', 'libgazebo_ros_init.so',
-             '-s', 'libgazebo_ros_factory.so', world],
-        cwd=[launch_dir], output='both')
-
-    start_gazebo_client_cmd = ExecuteProcess(
-        cmd=['gzclient'],
-        cwd=[launch_dir], output='both')
+    gz_robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sim_dir, "launch", "spawn_tb3_gps.launch.py")
+        ),
+        launch_arguments={
+            "use_sim_time": use_sim_time,
+            "robot_sdf": robot_sdf,
+            "x_pose": "2.0",
+            "y_pose": "-2.5",
+            "z_pose": "0.33",
+            "roll": "0.0",
+            "pitch": "0.0",
+            "yaw": "0.0",
+        }.items(),
+    )
 
     start_robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='both',
-        parameters=[{'robot_description': robot_description}])
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time, "robot_description": robot_description}
+        ],
+    )
 
-    # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Set gazebo up to find models properly
-    ld.add_action(set_gazebo_model_path_cmd)
-    ld.add_action(set_tb3_model_cmd)
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
 
-    # simulator launch
-    ld.add_action(start_gazebo_server_cmd)
-    ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(gz_robot)
+    ld.add_action(gazebo_server)
+    ld.add_action(gazebo_client)
 
-    # robot state publisher launch
     ld.add_action(start_robot_state_publisher_cmd)
-
     return ld
